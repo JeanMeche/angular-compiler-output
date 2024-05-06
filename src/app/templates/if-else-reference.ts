f175925b73d285778832a7bc1de6f1b3efb56e80
Tomasz Ducin <tomasz@ducin.dev>
2024-05-06 17:09:46 +0200

    more templates

---
+++ src/app/templates/if-else-reference.ts
@@ -0,0 +1,8 @@
+export const ifElseReferenceTemplate = 
+`<div *ngIf="isLoggedIn; else loggedOut">
+  Welcome back, friend.
+</div>
+
+<ng-template #loggedOut>
+  Please friend, login.
+</ng-template>`
