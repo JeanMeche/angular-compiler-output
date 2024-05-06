7c995f432b24d580dd15efeddfcb67828a687eed
Tomasz Ducin <tomasz@ducin.dev>
2024-05-06 18:11:31 +0200

    more templates

--- src/app/templates/index.ts
+++ src/app/templates/index.ts
@@ -1,9 +1,12 @@
 import { asyncPipeTemplate } from "./async-pipe";
+import { childComponentTemplate } from "./child-component";
 import { defaultTemplate } from "./default";
+import { eventBindingsTemplate } from "./event-bindings";
 import { forLoopsTemplate } from "./for-loops";
 import { ifElseReferenceTemplate } from "./if-else-reference";
 import { ifThenElseTemplate } from "./if-then-else";
 import { nestedNodesTemplate } from "./nested-nodes";
+import { NgModelTemplate } from "./ng-model";
 
 export type Template = Record<'label' | 'content', string>;
 
@@ -12,6 +15,9 @@ export const templates: Template[] = [
   { label: 'nested nodes', content: nestedNodesTemplate },
   { label: '@if/@else', content: ifThenElseTemplate },
   { label: '@for vs ngFor', content: forLoopsTemplate },
-  { label: 'NgIf/Else w/ template reference', content: ifElseReferenceTemplate },
-  { label: 'async pipe', content: asyncPipeTemplate }
+  { label: 'ngIf/else w/ template reference', content: ifElseReferenceTemplate },
+  { label: 'event bindings', content: eventBindingsTemplate },
+  { label: 'ng-model', content: NgModelTemplate },
+  { label: 'async pipe', content: asyncPipeTemplate },
+  { label: 'child component', content: childComponentTemplate },
 ];
